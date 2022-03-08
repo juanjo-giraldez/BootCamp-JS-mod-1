4bc48d2e381aab4cae4d0b814d0a89339b7fe64c
Juan Jose Giraldez Jurado <juanjose.giraldez@kairosds.com>
2022-03-08 21:20:03 +0100

    feat: complete exercixe

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-document.getElementById("name").value = "Bruce";
-document.getElementById("lastName").value = "Wayne";
+"use strict";
+
+const completeField = () => {
+  document.getElementById("name").value = "Bruce";
+  document.getElementById("lastName").value = "Wayne";
+  document.getElementById("avatar").src = "assets/bruce-wayne.jpeg";
+  console.log(document.getElementById("lastName").value);
+};
+
+const removeText = () => {
+    document.getElementById("name").value = null;
+    document.getElementById("lastName").value = null;
+    document.getElementById("avatar").src = null
+    console.log('no hay contenido');
+  };
